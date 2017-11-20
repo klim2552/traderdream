e4e7ae12685efc5385816800c408130b2c6f5dda
9KillU <32030330+9KillU@users.noreply.github.com>
2017-11-20 18:42:51 +1000

    Add files via upload

---
+++ terminal/FormOfTerminal/FormOfTerminal/MainWindow.xaml.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormOfTerminal
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эта программа создана для эффективной торговли на различных рынках!");
+        }
+
+        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_10(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Здесь будет указываться текущая версия программы и изменнения привнесенные с новой версией!");
+        }
+
+        private void MenuItem_Click_11(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Если у вас возникли вопросы, вы  можете связаться с нами по почте dreamtrader@gmail.com или задать вопрос на нашем сайте dreamtrader.com в разделе обсуждения!");
+        }
+
+        private void MenuItem_Click_12(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Здесь должно отображаться активизированна ли лицензия на данный момент и сроки истечения, если таковая есть!");
+        }
+
+        private void Кнопка1_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void Кнопка1_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void Кнопка1_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void Кнопка1_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void Кнопка1_Copy3_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void Кнопка1_Copy4_Click(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_13(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_14(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_15(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_16(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_17(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_18(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_19(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_20(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_21(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_22(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_23(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_24(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_25(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_26(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_27(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_28(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_29(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_30(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+
+        private void MenuItem_Click_31(object sender, RoutedEventArgs e)
+        {
+            Window a = new Window();
+            a.Show();
+        }
+    }
+}
